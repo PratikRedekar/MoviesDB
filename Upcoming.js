56b7f783c6c9c71dd96ba01697568ebcfb022639
PratikPrakashRedekar <108652084+PratikRedekar@users.noreply.github.com>
2022-12-31 13:38:55 +0530

    Add files via upload

---
+++ Upcoming.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useEffect, useState } from 'react';
+
+const API_URL = "https://api.themoviedb.org/3/movie/upcoming?api_key=cb313fb3cff02b0c9d284d5946f767ed";
+const Upcoming = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then((res) => res.json())
+      .then(data => {
+        console.log(data)
+        setMovies(data.results)
+      })
+  }, [])
+  
+  return (
+    <>
+      <div>Upcoming</div>
+      <div className='container'>
+        <div className='grid'>
+          {movies.map((movieReq) => <MovieBox key={movieReq.id} {...movieReq} />)}
+        </div></div>
+    </>
+  )
+}
+
+export default Upcoming;
