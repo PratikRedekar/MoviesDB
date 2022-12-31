223976d5fd342ec4eb7d3c9f905962839d4b8fac
PratikPrakashRedekar <108652084+PratikRedekar@users.noreply.github.com>
2022-12-31 13:49:06 +0530

    Add files via upload

--- src/TopRated.js
+++ src/TopRated.js
@@ -1,29 +1,29 @@
-import React from 'react'
-import { useEffect, useState } from 'react';
+// import React from 'react'
+// import { useEffect, useState } from 'react';
 
 
-const API_URL = "https://api.themoviedb.org/3/movie/top_rated?api_key=cb313fb3cff02b0c9d284d5946f767ed";
-const TopRated = () => {
-  const [movies, setMovies] = useState([]);
+// const API_URL = "https://api.themoviedb.org/3/movie/top_rated?api_key=cb313fb3cff02b0c9d284d5946f767ed";
+// const TopRated = () => {
+//   const [movies, setMovies] = useState([]);
 
-  useEffect(() => {
-    fetch(API_URL)
-      .then((res) => res.json())
-      .then(data => {
-        console.log(data)
-        setMovies(data.results)
-      })
-  }, [])
+//   useEffect(() => {
+//     fetch(API_URL)
+//       .then((res) => res.json())
+//       .then(data => {
+//         console.log(data)
+//         setMovies(data.results)
+//       })
+//   }, [])
   
-  return (
-    <>
-      <div>Top Rated</div>
-      <div className='container'>
-        <div className='grid'>
-          {movies.map((movieReq) => <MovieBox key={movieReq.id} {...movieReq} />)}
-        </div></div>
-    </>
-  )
-}
+//   return (
+//     <>
+//       <div>Top Rated</div>
+//       <div className='container'>
+//         <div className='grid'>
+//           {movies.map((movieReq) => <MovieBox key={movieReq.id} {...movieReq} />)}
+//         </div></div>
+//     </>
+//   )
+// }
 
-export default TopRated;
+// export default TopRated;
--- src/Upcoming.js
+++ src/Upcoming.js
@@ -1,28 +1,28 @@
-import React from 'react'
-import { useEffect, useState } from 'react';
+// import React from 'react'
+// import { useEffect, useState } from 'react';
 
-const API_URL = "https://api.themoviedb.org/3/movie/upcoming?api_key=cb313fb3cff02b0c9d284d5946f767ed";
-const Upcoming = () => {
-  const [movies, setMovies] = useState([]);
+// const API_URL = "https://api.themoviedb.org/3/movie/upcoming?api_key=cb313fb3cff02b0c9d284d5946f767ed";
+// const Upcoming = () => {
+//   const [movies, setMovies] = useState([]);
 
-  useEffect(() => {
-    fetch(API_URL)
-      .then((res) => res.json())
-      .then(data => {
-        console.log(data)
-        setMovies(data.results)
-      })
-  }, [])
+//   useEffect(() => {
+//     fetch(API_URL)
+//       .then((res) => res.json())
+//       .then(data => {
+//         console.log(data)
+//         setMovies(data.results)
+//       })
+//   }, [])
   
-  return (
-    <>
-      <div>Upcoming</div>
-      <div className='container'>
-        <div className='grid'>
-          {movies.map((movieReq) )}
-        </div></div>
-    </>
-  )
-}
+//   return (
+//     <>
+//       <div>Upcoming</div>
+//       <div className='container'>
+//         <div className='grid'>
+//           {movies.map((movieReq) )}
+//         </div></div>
+//     </>
+//   )
+// }
 
-export default Upcoming;
+// export default Upcoming;
